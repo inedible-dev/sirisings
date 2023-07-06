ff4dfed57a22c4f68de1976d62c4ca98f7d468cf
Wongkraiwich Chuenchomphu <wongkraiwich@inedible.dev>
2023-07-06 19:59:48 +0700

    Initial Commit

---
+++ sirisings/sirisingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  sirisingsApp.swift
+//  sirisings
+//
+//  Created by Wongkraiwich Chuenchomphu on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct sirisingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
